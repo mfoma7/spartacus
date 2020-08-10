e0aab24ed02d24c9ea5840d30c782d4a3a6b7584
Kristaps Ritins <ritins.kristaps@gmail.com>
2020-08-10 11:55:45 +0300

    heading image

--- page.php
+++ page.php
@@ -1,11 +1,11 @@
 <?php get_header(); ?>
 <div class="header-block-bg" <?php if (get_field('spartacus_header_block_bg', 'options')) echo 'style="background-color:' . get_field('spartacus_header_block_bg', 'options') . '"';  ?>>
     <div class="header-block">
-        <div class="page-heading container <?php if (is_front_page() && get_field('image_after_heading')) {
+        <div class="page-heading container <?php if (get_field('image_after_heading')) {
                                                 echo "heading-image";
                                             } ?>">
             <h1><?php the_title(); ?></h1>
-            <?php if (is_front_page() && get_field('image_after_heading')) {
+            <?php if (get_field('image_after_heading')) {
                 echo '<img src="' . get_field('image_after_heading') . '" alt="Country Flag">';
             } ?>
         </div>
